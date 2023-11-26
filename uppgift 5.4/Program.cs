44a7eec1985f0c82f9b51cc41d9d6bd7237efb0e
en301 <emilsjostrand1000@gmail.com>
2023-11-26 12:11:07 +0100

    Add project files.

---
+++ uppgift 5.4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uppgfit_5_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] namn = { "Anna", "David", "Eva", "Bengt", "Cecilia" };
+            Array.Sort(namn);
+            Console.WriteLine("Namn i bokstavsordning:");
+            foreach (string namnskriv in namn)
+            {
+                Console.WriteLine(namnskriv);
+            }
+        }
+    }
+}
